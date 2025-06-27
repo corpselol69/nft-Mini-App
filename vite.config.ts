478e6eeaedf4ba6628ad6ba7752baaeeeba2b1ac
d33sf0me <katekrokh@gmail.com>
2025-06-27 12:22:05 +0300

    deploy fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/reactjs-template/",
+  base: "/",
   css: {
     preprocessorOptions: {
       scss: {
