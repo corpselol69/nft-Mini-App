bd38a7461227487e18cb69d4af03b04b901c4f5f
d33sf0me <katekrokh@gmail.com>
2025-07-01 19:27:34 +0300

    feat: update MainLayout styles and structure for improved responsiveness and icon integration

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,17 +1,24 @@
-import { Outlet, NavLink } from "react-router-dom";
-import styles from "./MainLayout.module.scss";
-import { t } from "i18next";
-import { viewport } from "@telegram-apps/sdk";
-
 import { useEffect, useState } from "react";
+import { Outlet, NavLink } from "react-router-dom";
+import storeIcon from "@/static/icons/store.svg";
+import cardsIcon from "@/static/icons/cards_star.svg";
+
+import { t } from "i18next";
+import { miniApp, viewport } from "@telegram-apps/sdk";
+
+import styles from "./MainLayout.module.scss";
+import Icon from "@/components/common/Icon/Icon";
 
 export function MainLayout() {
   const [paddingTop, setPaddingTop] = useState(0);
+  const [activeTab, setActiveTab] = useState<"market" | "my-nft" | "profile">(
+    "market"
+  );
 
   useEffect(() => {
-    if (viewport.isFullscreen()) {
+    if (viewport.isFullscreen() && miniApp.isMounted()) {
       const safeArea = viewport.safeAreaInsets();
-      const totalPadding = safeArea.top + 44;
+      const totalPadding = safeArea.top + safeArea.bottom;
       setPaddingTop(totalPadding);
     }
   }, []);
@@ -26,19 +33,35 @@ export function MainLayout() {
         <NavLink
           to="/market"
           className={({ isActive }) => (isActive ? styles.active : undefined)}
+          onClick={() => setActiveTab("market")}
         >
+          <Icon
+            src={storeIcon}
+            color={activeTab === "market" ? "active" : "default"}
+          />
           <span>{t("market")}</span>
         </NavLink>
         <NavLink
           to="/my-nft"
           className={({ isActive }) => (isActive ? styles.active : undefined)}
+          onClick={() => setActiveTab("my-nft")}
         >
+          <Icon
+            src={cardsIcon}
+            color={activeTab === "my-nft" ? "active" : "default"}
+          />
           <span>{t("my_nft")}</span>
         </NavLink>
         <NavLink
           to="/profile"
           className={({ isActive }) => (isActive ? styles.active : undefined)}
+          onClick={() => setActiveTab("profile")}
         >
+          <div
+            className={`${styles.avatar} ${
+              activeTab === "profile" ? styles.active : ""
+            }`}
+          ></div>
           <span>{t("profile")}</span>
         </NavLink>
       </nav>
