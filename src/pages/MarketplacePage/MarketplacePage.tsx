a38c8ce4eaa887a374b918c328e7fcc15a211fc0
d33sf0me <katekrokh@gmail.com>
2025-07-01 13:40:32 +0300

    refactor: упрощение импорта и удаление неиспользуемых переменных в MarketplacePage

--- src/pages/MarketplacePage/MarketplacePage.tsx
+++ src/pages/MarketplacePage/MarketplacePage.tsx
@@ -1,21 +1,12 @@
-import { Tabs, Tab } from "@/components/Tabs/Tabs"
-import { useState, type FC } from "react"
-import styles from "./MarketplacePage.module.scss"
+import { Tabs, Tab } from "@/components/Tabs/Tabs";
+import { type FC } from "react";
+import styles from "./MarketplacePage.module.scss";
 
-import { NftGrid } from "@/components/NftGrid/NftGrid"
-import { t } from "i18next"
-import { Select } from "@/components/common/Select/Select"
-import { SELECT_DATA } from "../../components/StickersGrid/model/const"
-import { Input } from "@/components/common/Input/Input"
-import { SearchIcon } from "@/components/common/icons/SearchIcon"
-import { BottomSheet } from "@/components/common/BottomSheet/BottomSheet"
-import { NftBottomSheet } from "../../components/StickersGrid/ui/NftBottomSheet/NftBottomSheet"
-import { Outlet } from "react-router-dom"
+import { t } from "i18next";
+
+import { Outlet } from "react-router-dom";
 
 export const MarketplacePage: FC = () => {
-  const [value, setValue] = useState("")
-  const [selectedNftCard, setSelectedNftCard] = useState()
-
   return (
     <div className={styles.root}>
       <p className={styles.title}>{t("market")}</p>
@@ -25,5 +16,5 @@ export const MarketplacePage: FC = () => {
       </Tabs>
       <Outlet />
     </div>
-  )
-}
+  );
+};
