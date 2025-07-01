d1857ff3796de46c9d9293c90559fd5c09fa8457
d33sf0me <katekrokh@gmail.com>
2025-07-01 15:36:33 +0300

    fix: update MainLayout to use translation for navigation labels and adjust padding dynamically

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,10 +1,24 @@
 import { Outlet, NavLink } from "react-router-dom";
 import styles from "./MainLayout.module.scss";
+import { t } from "i18next";
+import { viewport } from "@telegram-apps/sdk";
+
+import { useEffect, useState } from "react";
 
 export function MainLayout() {
+  const [paddingTop, setPaddingTop] = useState(0);
+
+  useEffect(() => {
+    if (viewport.isFullscreen()) {
+      const safeArea = viewport.safeAreaInsets();
+      const totalPadding = safeArea.top + 44;
+      setPaddingTop(totalPadding);
+    }
+  }, []);
+
   return (
     <div className={styles.root}>
-      <main className={styles.content}>
+      <main className={styles.content} style={{ paddingTop }}>
         <Outlet />
       </main>
 
@@ -13,19 +27,19 @@ export function MainLayout() {
           to="/market"
           className={({ isActive }) => (isActive ? styles.active : undefined)}
         >
-          <span>Market</span>
+          <span>{t("market")}</span>
         </NavLink>
         <NavLink
           to="/my-nft"
           className={({ isActive }) => (isActive ? styles.active : undefined)}
         >
-          <span>My NFT</span>
+          <span>{t("my_nft")}</span>
         </NavLink>
         <NavLink
           to="/profile"
           className={({ isActive }) => (isActive ? styles.active : undefined)}
         >
-          <span>Profile</span>
+          <span>{t("profile")}</span>
         </NavLink>
       </nav>
     </div>
