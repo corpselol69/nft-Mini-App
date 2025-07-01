fedd7b2d473f300bb920e5560371f8db33f6fce5
d33sf0me <katekrokh@gmail.com>
2025-07-01 13:40:50 +0300

    refactor: удаление неиспользуемого кода из компонента App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -13,31 +13,6 @@ export function App() {
   const lp = useMemo(() => retrieveLaunchParams(), []);
   const isDark = useSignal(isMiniAppDark);
 
-  useEffect(() => {
-    if (window.Telegram?.WebApp) {
-      const inset =
-        window.Telegram.WebApp.viewportStableHeight -
-        window.Telegram.WebApp.viewportHeight;
-
-      console.log("Top inset:", inset);
-
-      const handleResize = () => {
-        const updatedInset =
-          window.Telegram.WebApp.viewportStableHeight -
-          window.Telegram.WebApp.viewportHeight;
-        console.log("Updated inset:", updatedInset);
-      };
-
-      window.Telegram.WebApp.onEvent("viewportChanged", handleResize);
-
-      return () => {
-        window.Telegram.WebApp.offEvent("viewportChanged", handleResize);
-      };
-    } else {
-      console.warn("Telegram WebApp not initialized");
-    }
-  }, []);
-
   return (
     <AppRoot
       appearance={isDark ? "dark" : "light"}
