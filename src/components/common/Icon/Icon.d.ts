de68587dcbce8e43c7e61d42828a90b66e39a7a6
d33sf0me <katekrokh@gmail.com>
2025-07-01 19:22:04 +0300

    feat: add Icon component with SVG fetching and styling

---
+++ src/components/common/Icon/Icon.d.ts
@@ -0,0 +1,9 @@
+import { HTMLAttributes } from "react";
+
+export interface IIconProps extends HTMLAttributes<HTMLSpanElement> {
+  src: string;
+  color?: "default" | "active";
+  width?: number | string;
+  height?: number | string;
+  className?: string;
+}
---
+++ src/components/common/Icon/Icon.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { IIconProps } from "./Icon.d";
+import styles from "./Icon.module.scss";
+
+const Icon: React.FC<IIconProps> = ({
+  src,
+  color = "default",
+
+  ...rest
+}) => {
+  const [svgContent, setSvgContent] = useState<string | null>(null);
+
+  useEffect(() => {
+    let isMounted = true;
+    fetch(src)
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(`Failed to load SVG: ${response.status}`);
+        return response.text();
+      })
+      .then((text) => {
+        if (isMounted) setSvgContent(text);
+      })
+      .catch((err) => console.error(err));
+
+    return () => {
+      isMounted = false;
+    };
+  }, [src]);
+
+  return svgContent ? (
+    <span
+      className={`${styles.icon} ${styles[color]}`}
+      {...rest}
+      dangerouslySetInnerHTML={{ __html: svgContent }}
+    />
+  ) : null;
+};
+
+export default Icon;
