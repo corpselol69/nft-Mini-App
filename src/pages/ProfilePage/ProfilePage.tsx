4775bd882c74a546a1ee89d67a6bc888c3b6a2cd
d33sf0me <katekrokh@gmail.com>
2025-07-03 19:33:21 +0300

    feat: update ProfilePage

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,14 +1,68 @@
-import type { FC } from "react";
-
-import { Page } from "@/components/Page.tsx";
-
-import styles from "./ProfilePage.module.scss";
+import type { FC } from "react"
+import { Page } from "@/components/Page.tsx"
+import styles from "./ProfilePage.module.scss"
+import Icon from "@/components/common/Icon/Icon"
+import { Button } from "@/components/common/Button/Button"
+import imgWalletIcon from "@/static/icons/wallet.svg"
+import imgAddIcon from "@/static/icons/icn-add.svg"
+import imgArrowUp from "@/static/icons/arrow-up.svg"
+import imgDiversity from "@/static/icons/diversity.svg"
+import imgChevronForward from "@/static/icons/chevron_forward.svg"
 
 export const ProfilePage: FC = () => {
   return (
     <Page back={false}>
-      <div className={styles.ios}>Hello iOS</div>
-      <div className={styles.android}>Hello Android</div>
+      <div className={styles.profilePage}>
+        <div className={styles.header}>
+          <div className={styles.userInfo}>
+            <div className={styles.avatar} />
+            <span className={styles.username}>@username</span>
+          </div>
+          <Button type="text">
+            <Icon src={imgWalletIcon} />
+            <span>Подключить кошелёк</span>
+          </Button>
+        </div>
+
+        <div className={styles.balanceBlock}>
+          <span className={styles.balanceLabel}>Баланс</span>
+          <span className={styles.balanceValue}>
+            <span className={styles.balanceAmount}>0</span>
+            <span className={styles.balanceCurrency}> TON</span>
+          </span>
+        </div>
+
+        <div className={styles.actions}>
+          <Button type="vertical" size="large">
+            <Icon src={imgAddIcon} className={styles.actionIcon} />
+            Пополнить
+          </Button>
+          <Button type="vertical" size="large">
+            <Icon src={imgArrowUp} className={styles.actionIcon} />
+            Вывести
+          </Button>
+        </div>
+
+        <div className={styles.referralBlock}>
+          <span className={styles.referralLabel}>реферальная система</span>
+          <div className={styles.referralCard}>
+            <Icon src={imgDiversity} className={styles.actionIcon} />
+            <span>Заработать на приглашениях</span>
+            <Icon src={imgChevronForward} className={styles.chevron} />
+          </div>
+        </div>
+
+        <div className={styles.historyBlock}>
+          <span className={styles.historyLabel}>История транзакций</span>
+          <div className={styles.historyCard}>
+            <span>
+              Совершите свою первую
+              <br />
+              транзакцию
+            </span>
+          </div>
+        </div>
+      </div>
     </Page>
-  );
-};
+  )
+}
