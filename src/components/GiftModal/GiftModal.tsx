566198de146c543d7957c4990340ab482281b92a
d33sf0me <katekrokh@gmail.com>
2025-07-03 20:45:53 +0300

    fix: refactor GiftModal

--- src/components/GiftModal/GiftModal.tsx
+++ src/components/GiftModal/GiftModal.tsx
@@ -1,44 +1,116 @@
-import { type FC } from "react";
+import { useMemo, type FC } from "react"
 
-import { NftBottomSheet } from "@/components/StickersGrid/ui/NftBottomSheet/NftBottomSheet";
-import { BottomSheet } from "../common/BottomSheet/BottomSheet";
+import { BottomSheet } from "../common/BottomSheet/BottomSheet"
 
-import shareIcon from "@/static/icons/shareIcon.svg";
-import { shareURL } from "@telegram-apps/sdk";
-import Icon from "../common/Icon/Icon";
-import { Button } from "@/components/common/Button/Button";
-import { useNavigate, useParams } from "react-router-dom";
+import { Button } from "@/components/common/Button/Button"
+import { useNavigate, useParams } from "react-router-dom"
+import { IGiftItem } from "../GiftsGrid/types"
+import { GiftPriceTooltipContent } from "@/pages/GiftPage/ui/GiftPriceTooltipContent/GiftPriceTooltipContent"
+import { useBottomSheet } from "@/providers/BottomSheetProvider/BottomSheetProvider"
+import { Tooltip } from "@/components/common/Tooltip/Tooltip"
+import { QuestionMarkIcon } from "../StickersGrid/ui/NftDetailsTable/QuestionMarkIcon"
+import bdayImg from "@/static/placeholders/bday.png"
+import tonIcon from "@/static/icons/icn-S_ton.svg"
 
-export const GiftModal: FC = () => {
-  const navigate = useNavigate();
-  const { id } = useParams<{ id: string }>();
+import styles from "./GiftModal.module.scss"
+import { GIFT_ACTIONS } from "@/pages/GiftPage/model/const"
+import { ActionButton } from "@/pages/GiftPage/ui/ActionButton/ActionButton"
+import { GiftDetailsRows } from "@/pages/GiftPage/ui/GiftDetailsRows/GiftDetailsRows"
+import { GiftImageWithText } from "@/pages/GiftPage/ui/GiftImageWithText/GiftImageWithText"
+import { t } from "i18next"
+import { BuyNftBottomSheet } from "../BuyNftBottomSheet/BuyNftBottomSheet"
+
+type Props = {
+  gift: IGiftItem
+}
+
+export const GiftModal: FC<Props> = ({ gift }) => {
+  const navigate = useNavigate()
+  const { id } = useParams<{ id: string }>()
+
+  const { openSheet, closeAll } = useBottomSheet()
+
+  const priceContent = useMemo(
+    () => (
+      <span className={styles.priceRow}>
+        <span>{gift.price} TON</span>
+        <Tooltip content={<GiftPriceTooltipContent price={gift.price} />}>
+          <QuestionMarkIcon />
+        </Tooltip>
+      </span>
+    ),
+    [gift.price]
+  )
 
   return (
-    <BottomSheet
-      open={true}
-      onClose={() => navigate(-1)}
-      renderLeftHeader={() => (
-        <Button
-          type="icon"
-          onClick={() => {
-            const url = window.location.href;
-            if (shareURL.isAvailable()) {
-              shareURL(url, `Смотри этот стикер #${id}`);
-            }
-          }}
-        >
-          <Icon src={shareIcon} />
-        </Button>
-      )}
-    >
-      <NftBottomSheet
-        availableBalance="12,4"
-        collection="Bored Stickers"
-        number={`#${id}`}
-        imgLink=""
-        issued="9 999/9 999"
-        price="95"
-      />
+    <BottomSheet open={true} onClose={() => navigate(-1)}>
+      <div className={styles.detailGiftSheet}>
+        <GiftImageWithText imgSrc={bdayImg} name={gift.name} id={gift.id} />
+
+        <div className={styles.detailGiftSheetActions}>
+          {GIFT_ACTIONS.map(el => (
+            <ActionButton
+              key={el.label}
+              onClick={el.onClick}
+              icon={el.icon}
+              className={styles.flexItem}
+            >
+              {el.label}
+            </ActionButton>
+          ))}
+        </div>
+
+        <GiftDetailsRows gift={gift} priceContent={priceContent} />
+
+        <div className={styles.availableBalanceWrapper}>
+          <div className={styles.availableBalanceText}>
+            <span>{t("available_balance")}</span>
+          </div>
+          <div className={styles.availableBalanceValue}>
+            12,4 <img src={tonIcon} alt="TON" />
+          </div>
+        </div>
+
+        <div className={styles.actionButtonsWrapper}>
+          <div>
+            <Button
+              type="secondary"
+              size="large"
+              className={styles.mainCartButton}
+            >
+              <span className={styles.cartButtonText}>Добавить в корзину</span>
+            </Button>
+          </div>
+          <div className={styles.buyButtonWrapper}>
+            <Button
+              type="primary"
+              size="large"
+              onClick={() =>
+                openSheet(
+                  <BuyNftBottomSheet
+                    nftPrice={gift.price}
+                    onBuy={() => {}}
+                    onCancel={closeAll}
+                    quantity="1"
+                  />,
+                  {
+                    renderLeftHeader() {
+                      return (
+                        <span className={styles.bottomSheetTitle}>
+                          Покупка NFT
+                        </span>
+                      )
+                    },
+                  }
+                )
+              }
+            >
+              Купить за {gift.price}
+              <img src={tonIcon} alt="TON" />
+            </Button>
+          </div>
+        </div>
+      </div>
     </BottomSheet>
-  );
-};
+  )
+}
