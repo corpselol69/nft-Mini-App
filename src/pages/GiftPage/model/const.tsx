4b24c61624baef49486c6635f61157bf0c14c5a9
d33sf0me <katekrokh@gmail.com>
2025-07-03 14:19:28 +0300

    refactor: replace icon components with SVG imports and update GIFT_ACTIONS structure

--- src/pages/GiftPage/model/const.tsx
+++ src/pages/GiftPage/model/const.tsx
@@ -1,26 +1,28 @@
-import { StatusIcon } from "@/static/icons/StatusIcon";
-import { TelegramIcon } from "@/static/icons/TelegramIcon";
+import Icon from "@/components/common/Icon/Icon";
+import telegramIcon from "@/static/icons/telegramIcon.svg";
+import statusIcon from "@/static/icons/statusIcon.svg";
+import searchIcon from "@/static/icons/searchIcon.svg";
 
 export const GIFT_ACTIONS = [
   {
-    icon: <TelegramIcon />,
+    icon: <Icon src={telegramIcon} />,
     label: "Посмотреть",
     onClick: () => {
       alert("Посмотреть");
     },
   },
   {
-    icon: <StatusIcon />,
+    icon: <Icon src={statusIcon} />,
     label: "Статус",
     onClick: () => {
       alert("Статус");
     },
   },
-  // {
-  //   icon: <ShareIcon />,
-  //   label: "Поделиться",
-  //   onClick: () => {
-  //     alert("Поделиться")
-  //   },
-  // },
+  {
+    icon: <Icon src={searchIcon} />,
+    label: "Поделиться",
+    onClick: () => {
+      alert("Поделиться");
+    },
+  },
 ];
