b14f6a78f8b96e06c270efe614b06330a956a760
d33sf0me <katekrokh@gmail.com>
2025-07-03 15:52:33 +0300

    refactor: improve code formatting and fix onClick handler in NftGrid component

--- src/components/NftGrid/NftGrid.tsx
+++ src/components/NftGrid/NftGrid.tsx
@@ -1,6 +1,6 @@
-import React from "react"
-import styles from "./NftGrid.module.scss"
-import { NftCard } from "../NftCard/NftCard"
+import React from "react";
+import styles from "./NftGrid.module.scss";
+import { NftCard } from "../NftCard/NftCard";
 
 // Пример данных для карточек
 const mockNfts = [
@@ -10,19 +10,19 @@ const mockNfts = [
   { id: 4 },
   { id: 5 },
   { id: 6 },
-]
+];
 
 type TProps = {
   //FIXME: добавить нормальные типы
-  onNftClick: (nft: any) => void
-}
+  onNftClick: (nft: any) => void;
+};
 
 export const NftGrid: React.FC<TProps> = ({ onNftClick }) => {
   return (
     <div className={styles.grid}>
-      {mockNfts.map(nft => (
-        <NftCard key={nft.id} onClick={() => onNftClick(nft)} />
+      {mockNfts.map((nft) => (
+        <NftCard key={nft.id} onClick={() => onNftClick(nft.id)} />
       ))}
     </div>
-  )
-}
+  );
+};
