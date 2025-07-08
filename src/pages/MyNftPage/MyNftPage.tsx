e8dde31beeac9e89357472ecdfda8dc8a2adfabc
d33sf0me <katekrokh@gmail.com>
2025-07-08 10:20:21 +0300

    refactor: restructure MyNftPage component and styles for improved readability

--- src/pages/MyNftPage/MyNftPage.tsx
+++ src/pages/MyNftPage/MyNftPage.tsx
@@ -1,16 +1,23 @@
-import type { FC } from "react";
+import type { FC } from "react"
 
-import { Page } from "@/components/Page.tsx";
-
-import styles from "./MyNftPage.module.scss";
-import { Button } from "@/components/common/Button/Button";
+import styles from "./MyNftPage.module.scss"
+import { Button } from "@/components/common/Button/Button"
+import { Tabs, Tab } from "@/components/common/Tabs/Tabs"
+import { t } from "i18next"
+import { Outlet } from "react-router-dom"
 
 export const MyNftPage: FC = () => {
   return (
-    <Page back={false}>
-      <Button type="text" style={{ flex: 1 }} size="medium">
-        <span className={styles.price}>Button</span>
-      </Button>
-    </Page>
-  );
-};
+    <div className={styles.root}>
+      <div className={styles.header}>
+        <p className={styles.title}>{t("my_nft")}</p>
+        <Button>{t("sell_nft")}</Button>
+      </div>
+      <Tabs>
+        <Tab to="/my-nft/stickers">{t("stickers")}</Tab>
+        <Tab to="/my-nft/gifts">{t("gifts")}</Tab>
+      </Tabs>
+      <Outlet context={{ isMarket: false }} />
+    </div>
+  )
+}
