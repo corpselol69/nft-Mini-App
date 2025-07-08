233c82434f5d678cdf87e4317ce107c13d390d4a
d33sf0me <katekrokh@gmail.com>
2025-07-08 13:55:39 +0300

    feat: implement image display and price row in StickerModal; remove unused NftBottomSheet components

--- src/components/StickerModal/StickerModal.tsx
+++ src/components/StickerModal/StickerModal.tsx
@@ -1,47 +1,144 @@
-import { type FC } from "react";
+import { useCallback, useMemo, useState, type FC } from "react"
 
-import { NftBottomSheet } from "@/components/StickersGrid/ui/NftBottomSheet/NftBottomSheet";
-import { BottomSheet } from "../common/BottomSheet/BottomSheet";
+import { BottomSheet } from "../common/BottomSheet/BottomSheet"
 
-import shareIcon from "@/static/icons/shareIcon.svg";
-import { shareURL } from "@telegram-apps/sdk";
-import Icon from "../common/Icon/Icon";
-import { Button } from "@/components/common/Button/Button";
-import { useNavigate, useParams } from "react-router-dom";
+import shareIcon from "@/static/icons/shareIcon.svg"
+import { shareURL } from "@telegram-apps/sdk"
+import Icon from "../common/Icon/Icon"
+import { Button } from "@/components/common/Button/Button"
+import { useNavigate, useOutletContext, useParams } from "react-router-dom"
 
-import styles from "./StickerModal.module.scss";
+import styles from "./StickerModal.module.scss"
+import { useBottomSheet } from "@/providers/BottomSheetProvider/BottomSheetProvider"
+import { ModalButtonsWrapper } from "../common/ModalButtonsWrapper/ModalButtonsWrapper"
+import monkeyImg from "@/static/placeholders/monkey.png"
+import { PriceTooltip } from "../common/PriceTooltip/PriceTooltip"
+import { DetailsTable } from "../common/DetailsTable/DetailsTable"
+import { BuyNftBottomSheet } from "../Modals/BuyNftBottomSheet/BuyNftBottomSheet"
+
+// Пример данных для карточек
+const mockNft = {
+  id: 1,
+  title: "Bored Stickers",
+  price: 90,
+  url: monkeyImg,
+  collection: "Bored Stickers",
+  number: "123123",
+  issued: "9 999/9 999",
+}
+
+const isInCart = true //  заменить на реальное состояние из контекста или хука
 
 export const StickerModal: FC = () => {
-  const navigate = useNavigate();
-  const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate()
+  const { openSheet, closeAll } = useBottomSheet()
+  const { id } = useParams<{ id: string }>()
+  const { isMarket } = useOutletContext<{ isMarket: boolean }>()
+
+  const [open, setOpen] = useState(true)
+
+  const handleBuy = () => {
+    openSheet(
+      <BuyNftBottomSheet
+        nftPrice={mockNft.price}
+        onBuy={() => {
+          closeAll()
+        }}
+        onCancel={closeAll}
+        quantity="1"
+      />,
+      {
+        renderLeftHeader() {
+          return <span className={styles.bottomSheetTitle}>Покупка NFT</span>
+        },
+      }
+    )
+    handleCloseGiftModal()
+  }
+
+  const handleAddToCart = () => {
+    // логика добавления в корзину
+  }
+
+  const handleViewCart = () => {
+    // логика открытия корзины
+  }
+
+  const handleWithdraw = () => {
+    // логика снятия с продажи
+  }
+
+  const handlePutOnSale = () => {
+    // логика выставления на продажу
+  }
+
+  const handleCloseGiftModal = useCallback(() => {
+    navigate(-1)
+    setOpen(false)
+  }, [])
+
+  const priceContent = (
+    <span className={styles.priceRow}>
+      <span>{mockNft.price} TON</span>
+      <PriceTooltip price={mockNft.price} />
+    </span>
+  )
+
+  const rows = useMemo(() => {
+    return [
+      { label: "Коллекция", value: mockNft.collection },
+      { label: "Номер", value: mockNft.number },
+      { label: "Выпущено", value: mockNft.issued },
+      { label: "Цена", value: priceContent },
+    ]
+  }, [mockNft.collection, mockNft.number, mockNft.issued, mockNft.price])
 
   return (
     <BottomSheet
       open={true}
       onClose={() => navigate(-1)}
-      renderLeftHeader={() => (
-        <Button
-          type="icon"
-          className={styles.shareButton}
-          onClick={() => {
-            const url = `https://t.me/d33sf0mebot/mytest/#/market/stickers/${id}`; //заменить url из .env
-            if (shareURL.isAvailable()) {
-              shareURL(url, `Смотри этот стикер #${id}`);
-            }
-          }}
-        >
-          <Icon src={shareIcon} />
-        </Button>
-      )}
+      renderLeftHeader={
+        isMarket
+          ? () => (
+              <Button
+                type="icon"
+                className={styles.shareButton}
+                onClick={() => {
+                  const url = `https://t.me/d33sf0mebot/mytest/#/market/stickers/${id}`
+                  if (shareURL.isAvailable()) {
+                    shareURL(url, `Смотри этот стикер #${id}`)
+                  }
+                }}
+              >
+                <Icon src={shareIcon} />
+              </Button>
+            )
+          : undefined
+      }
     >
-      <NftBottomSheet
-        availableBalance="12,4"
-        collection="Bored Stickers"
-        number={`#${id}`}
-        imgLink=""
-        issued="9 999/9 999"
-        price="95"
+      <div className={styles.imageWrapper}>
+        <img
+          height={210}
+          width={210}
+          className={styles.nftImage}
+          src={monkeyImg}
+          alt="STICKER"
+        />
+      </div>
+
+      <DetailsTable rows={rows} />
+
+      <ModalButtonsWrapper
+        variant={isMarket ? "buy" : "remove from sale"}
+        price={90}
+        balance={0}
+        isInCart
+        onMainClick={isMarket ? handleBuy : handleWithdraw}
+        onSecondaryClick={
+          isMarket && isInCart ? handleViewCart : handlePutOnSale
+        }
+        onCartClick={handleAddToCart}
       />
     </BottomSheet>
-  );
-};
+  )
+}
--- src/components/StickersGrid/ui/NftBottomSheet/NftBottomSheet.d.ts
+++
@@ -1,8 +0,0 @@
-export type NftBottomSheetProps = {
-  collection: string
-  number: string
-  issued: string
-  price: number
-  imgLink: string
-  availableBalance: string
-}
