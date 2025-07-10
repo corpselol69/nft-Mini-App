216e6312c0a6bc56431ab89d38adb60a07c0644b
d33sf0me <katekrokh@gmail.com>
2025-07-10 14:48:41 +0300

    feat: integrate Wallet component and connect wallet functionality in ProfilePage

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,20 +1,43 @@
 import type { FC } from "react"
+import { useState } from "react"
 import { Page } from "@/components/Page.tsx"
 import styles from "./ProfilePage.module.scss"
 import Icon from "@/components/common/Icon/Icon"
 import { Button } from "@/components/common/Button/Button"
-import imgWalletIcon from "@/static/icons/wallet.svg"
+import { Wallet } from "@/components/common/Wallet"
 import imgAddIcon from "@/static/icons/icn-add.svg"
 import imgArrowUp from "@/static/icons/arrow-up.svg"
 import imgDiversity from "@/static/icons/diversity.svg"
 import imgChevronForward from "@/static/icons/chevron_forward.svg"
 import { Outlet, useNavigate } from "react-router-dom"
 import { Avatar } from "@/components/common/Avatar/Avatar.tsx"
+import { useTonConnectUI } from "@tonconnect/ui-react"
 
 export const ProfilePage: FC = () => {
   const navigate = useNavigate()
+  const [isWalletConnected, setIsWalletConnected] = useState(false)
+  const [walletAddress, setWalletAddress] = useState("")
+  const [walletBalance, setWalletBalance] = useState("")
+
+  const [tonConnectUI] = useTonConnectUI()
+
   const handleReferralClick = () => {
     navigate("ref")
+  }
+
+  const handleConnectWallet = () => {
+    // Симуляция подключения кошелька
+    tonConnectUI.openModal()
+    setIsWalletConnected(true)
+    setWalletAddress("UQD6naYvQrlcfsaAbDOFgBiNRRlLckIxdUEhd0aGF1pKo6Q")
+    setWalletBalance("214")
+  }
+
+  const handleCopyWallet = () => {
+    if (walletAddress) {
+      navigator.clipboard.writeText(walletAddress)
+      //  добавить уведомление о копировании
+    }
   }
 
   return (
@@ -25,10 +48,13 @@ export const ProfilePage: FC = () => {
             <Avatar />
             <span className={styles.username}>@username</span>
           </div>
-          <Button type="text">
-            <Icon src={imgWalletIcon} />
-            <span>Подключить кошелёк</span>
-          </Button>
+          <Wallet
+            address={walletAddress}
+            balance={walletBalance}
+            isConnected={isWalletConnected}
+            onConnect={handleConnectWallet}
+            onCopy={handleCopyWallet}
+          />
         </div>
 
         <div className={styles.balanceBlock}>
