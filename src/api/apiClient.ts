528bf1e6abd230287c8f638a0765362734009463
d33sf0me <katekrokh@gmail.com>
2025-07-11 12:06:28 +0300

    feat: add apiClient for handling API requests with axios

---
+++ src/api/apiClient.ts
@@ -0,0 +1,30 @@
+import axios from "axios"
+
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 100000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+
+apiClient.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem("authToken")
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  error => Promise.reject(error)
+)
+
+apiClient.interceptors.response.use(
+  response => response,
+  error => {
+    console.error("API Error:", error)
+    return Promise.reject(error)
+  }
+)
+
+export default apiClient
