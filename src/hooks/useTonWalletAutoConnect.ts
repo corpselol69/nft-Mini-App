cfe931e64cd0ece91f079a4b46e9c1eef03db3e1
d33sf0me <katekrokh@gmail.com>
2025-07-15 11:35:18 +0300

    feat: add useTonWalletAutoConnect hook for automatic wallet connection

---
+++ src/hooks/useTonWalletAutoConnect.ts
@@ -0,0 +1,20 @@
+import { useEffect } from "react"
+import { useAppSelector } from "@/hooks/useRedux"
+import TonConnect from "@tonconnect/sdk"
+import { useTonWallet } from "@tonconnect/ui-react"
+
+export const useTonWalletAutoConnect = () => {
+  const connector = new TonConnect()
+  const wallet = useAppSelector(state => state.wallet.data)
+  const walletTon = useTonWallet()
+
+  useEffect(() => {
+    const autoConnect = async () => {
+      if (wallet?.address && !connector.connected) {
+        await connector.restoreConnection()
+        console.log("TonConnect auto-connected", connector)
+      }
+    }
+    autoConnect()
+  }, [wallet?.address])
+}
