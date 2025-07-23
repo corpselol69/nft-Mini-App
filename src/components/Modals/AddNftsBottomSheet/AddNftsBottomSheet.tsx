c5ee0bc772ebe677489d3643a79f7c5286fac85c
d33sf0me <katekrokh@gmail.com>
2025-07-23 13:58:53 +0300

    feat: implement AddNftsBottomSheet component with styling and functionality

---
+++ src/components/Modals/AddNftsBottomSheet/AddNftsBottomSheet.tsx
@@ -0,0 +1,34 @@
+import { FC, useState } from "react"
+
+import styles from "./AddNftsBottomSheet.module.scss"
+import { Tabs } from "@/components/common/Tabs/Tabs"
+import { t } from "i18next"
+import searchIcon from "@/static/icons/searchIcon.svg"
+
+import Icon from "@/components/common/Icon/Icon"
+import { Input } from "@/components/common/Input/Input"
+
+type Props = {
+  list: string[]
+}
+
+export const AddNftsBottomSheet: FC<Props> = ({ list }) => {
+  const [selected, setSelected] = useState("stickers")
+  const [value, setValue] = useState("")
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <Tabs type="select" selected={selected} onSelect={setSelected}>
+          <Tabs.Tab to="stickers">{t("stickers")}</Tabs.Tab>
+          <Tabs.Tab to="gifts">{t("gifts")}</Tabs.Tab>
+        </Tabs>
+        <Input
+          icon={<Icon src={searchIcon} />}
+          placeholder="Поиск по названию или ID"
+          className={styles.input}
+        />
+      </div>
+    </div>
+  )
+}
--- src/pages/MyNftPage/MyNftPage.tsx
+++ src/pages/MyNftPage/MyNftPage.tsx
@@ -5,13 +5,30 @@ import { Button } from "@/components/common/Button/Button"
 import { Tabs, Tab } from "@/components/common/Tabs/Tabs"
 import { t } from "i18next"
 import { Outlet } from "react-router-dom"
+import { AddNftsBottomSheet } from "@/components/Modals/AddNftsBottomSheet/AddNftsBottomSheet"
+import { useBottomSheet } from "@/providers/BottomSheetProvider/BottomSheetProvider"
 
 export const MyNftPage: FC = () => {
+  const { openSheet, closeAll } = useBottomSheet()
+
+  const handleSellNft = () => {
+    openSheet(<AddNftsBottomSheet list={[]} />, {
+      bottomSheetTitle: t("adding_nfts"),
+      buttons: (
+        <div className={styles.actionButtonsWrapper}>
+          <Button type="secondary" size="large">
+            Отменить
+          </Button>
+          <Button size="large">Выбрать</Button>
+        </div>
+      ),
+    })
+  }
   return (
     <div className={styles.root}>
       <div className={styles.header}>
         <p className={styles.title}>{t("my_nft")}</p>
-        <Button>{t("buttons.sell_nft")}</Button>
+        <Button onClick={handleSellNft}>{t("buttons.sell_nft")}</Button>
       </div>
       <Tabs>
         <Tab to="/my-nft/stickers">{t("stickers")}</Tab>
