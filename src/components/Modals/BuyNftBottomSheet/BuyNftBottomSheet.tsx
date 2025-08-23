309df8f16c2590acf68eaa24910ccbbf3b2ba52c
d33sf0me <katekrokh@gmail.com>
2025-08-23 14:39:00 +0300

    refactor: update BuyNftBottomSheet to use NftListItem props for better data handling

--- src/components/Modals/BuyNftBottomSheet/BuyNftBottomSheet.tsx
+++ src/components/Modals/BuyNftBottomSheet/BuyNftBottomSheet.tsx
@@ -2,36 +2,28 @@ import { FC } from "react"
 
 import styles from "./BuyNftBottomSheet.module.scss"
 import { AvailableBalance } from "@/components/common/AvailableBalance/AvailableBalance"
+import { NftListItem } from "@/types/market"
 
 type Props = {
-  url: string
-  title: string
-  id: string
-  price: number
+  nft: NftListItem
   availableBalance: string
 }
 
-export const BuyNftBottomSheet: FC<Props> = ({
-  availableBalance,
-  id,
-  url,
-  price,
-  title,
-}) => {
+export const BuyNftBottomSheet: FC<Props> = ({ availableBalance, nft }) => {
   return (
     <div className={styles.addToCartBottomSheetWrapper}>
       <div className={styles.titleAndPriceWrapper}>
         <div className={styles.imageAndTitleWrapper}>
           <div className={styles.imageWrapper}>
-            <img src={url} />
+            <img src={nft.preview} />
           </div>
           <div className={styles.titleAdnIdWrapper}>
-            <span className={styles.titleText}>{title}</span>
-            <span className={styles.idText}>#{id}</span>
+            <span className={styles.titleText}>{nft.title}</span>
+            <span className={styles.idText}>#{nft.number}</span>
           </div>
         </div>
         <div>
-          <span className={styles.priceText}>{price}</span>{" "}
+          <span className={styles.priceText}>{nft.price}</span>{" "}
           <span className={styles.priceValue}>TON</span>
         </div>
       </div>
