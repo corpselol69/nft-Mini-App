db2de3b69eecb4aebb048ea2830c7a5b6901a5ee
d33sf0me <katekrokh@gmail.com>
2025-08-23 11:58:18 +0300

    feat: add NftPreview component with styles and props interface

---
+++ src/components/common/NftPreview/NftPreview.d.ts
@@ -0,0 +1,7 @@
+import React from "react"
+
+export interface NftPreviewProps {
+  background_url: string
+  preview_url: string
+  className?: string
+}
---
+++ src/components/common/NftPreview/NftPreview.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react"
+import styles from "./NftPreview.module.scss"
+import { NftPreviewProps } from "./NftPreview.d"
+import cs from "classnames"
+
+export const NftPreview: FC<NftPreviewProps> = ({
+  background_url,
+  preview_url,
+  className,
+}) => {
+  return (
+    <div
+      className={cs(styles.preview, className)}
+      style={{ backgroundImage: `url(${background_url})` }}
+    >
+      <img className={styles.pic} src={preview_url} alt={"nft preview"} />
+    </div>
+  )
+}
