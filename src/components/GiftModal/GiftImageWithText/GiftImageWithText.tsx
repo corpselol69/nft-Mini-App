bfe04d52492a64c9510470bd03f7605920b33c73
d33sf0me <katekrokh@gmail.com>
2025-08-23 12:00:32 +0300

    feat: update GiftImageWithText component to use background_url and animation_url props

--- src/components/GiftModal/GiftImageWithText/GiftImageWithText.tsx
+++ src/components/GiftModal/GiftImageWithText/GiftImageWithText.tsx
@@ -1,19 +1,46 @@
 import React from "react"
 import styles from "./GiftImageWithText.module.scss"
+import StickerAnimation from "@/components/common/Lottie/Lottie"
+
+import SkeletonNode from "antd/es/skeleton/Node"
 
 type Props = {
-  imgSrc: string
+  background_url: string
+  animation_url: string
   name: string
-  id: string | number
+  number: number
 }
 
-export const GiftImageWithText: React.FC<Props> = ({ imgSrc, name, id }) => {
+export const GiftImageWithText: React.FC<Props> = ({
+  background_url,
+  animation_url,
+  name,
+  number,
+}) => {
   return (
     <div className={styles.detailGiftSheetImageWrapper}>
-      <img src={imgSrc} width={210} height={210} alt={name} />
+      {background_url ? (
+        <img className={styles.patternSvg} src={background_url} alt={name} />
+      ) : (
+        <SkeletonNode
+          active
+          className={styles.patternSvg}
+          style={{
+            position: "relative",
+            width: 210,
+            height: 210,
+            borderRadius: 24,
+          }}
+        />
+      )}
+
+      <StickerAnimation file={animation_url} />
+
       <div className={styles.detailGiftSheetImageText}>
-        <span className={styles.detailGiftSheetName}>{name}</span>
-        <span className={styles.detailGiftSheetSubName}>#{id}</span>
+        {name && <span className={styles.detailGiftSheetName}>{name}</span>}
+        {number && (
+          <span className={styles.detailGiftSheetSubName}>#{number}</span>
+        )}
       </div>
     </div>
   )
