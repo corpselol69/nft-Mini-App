40ed60916553192e4ffa4be1df62f5634e463603
d33sf0me <katekrokh@gmail.com>
2025-08-24 15:09:59 +0300

    feat: add tagTypes for Me, Wallet, and Transactions in api configuration

--- src/api/api.ts
+++ src/api/api.ts
@@ -4,5 +4,7 @@ import { axiosBaseQuery } from "./axiosBaseQuery"
 export const api = createApi({
   reducerPath: "api",
   baseQuery: axiosBaseQuery(),
+  tagTypes: ["Me", "Wallet", "Transactions"] as const, // 👈 вот это важно
+
   endpoints: () => ({}), // будет расширяться
 })
