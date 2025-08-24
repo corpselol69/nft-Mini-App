722fab0526a186efcd817823b254f2fcfb366d2e
d33sf0me <katekrokh@gmail.com>
2025-08-24 15:10:42 +0300

    feat: remove walletSlice integration from walletApi for improved API handling

--- src/api/endpoints/wallets.ts
+++ src/api/endpoints/wallets.ts
@@ -1,6 +1,5 @@
 import { Wallet, WalletLinkPayload } from "@/types/wallet"
 import { api } from "../api"
-import { setWallet, setWalletError } from "@/slices/walletSlice"
 
 export const walletApi = api.injectEndpoints({
   endpoints: builder => ({
@@ -10,23 +9,14 @@ export const walletApi = api.injectEndpoints({
         method: "POST",
         data: payload,
       }),
-      async onQueryStarted(_, { dispatch, queryFulfilled }) {
-        try {
-          await queryFulfilled
-          const res = await dispatch(
-            walletApi.endpoints.getWallet.initiate()
-          ).unwrap()
-          dispatch(setWallet(res[0])) // обновим walletSlice всем объектом
-        } catch (error) {
-          dispatch(setWalletError("Не удалось получить данные кошелька"))
-        }
-      },
+      invalidatesTags: ["Wallet"],
     }),
     unlinkWallet: builder.mutation<void, { wallet_id: string }>({
       query: ({ wallet_id }) => ({
         url: `/wallets/${wallet_id}`,
         method: "DELETE",
       }),
+      invalidatesTags: ["Wallet"],
     }),
     getWallet: builder.query<Wallet[], void>({
       query: () => ({
@@ -34,14 +24,7 @@ export const walletApi = api.injectEndpoints({
         method: "GET",
       }),
       keepUnusedDataFor: 600,
-      async onQueryStarted(_, { dispatch, queryFulfilled }) {
-        try {
-          const { data } = await queryFulfilled
-          dispatch(setWallet(data[0])) // синхронизируем Redux-слайс
-        } catch (e) {
-          dispatch(setWalletError("Не удалось получить кошелек"))
-        }
-      },
+      providesTags: ["Wallet"],
     }),
   }),
 })
