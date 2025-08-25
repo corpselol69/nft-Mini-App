81c5e51317ef4bd5c2c7159886534c738d81bca0
d33sf0me <katekrokh@gmail.com>
2025-08-25 15:41:52 +0300

    add iconPosition

--- src/components/common/Input/Input.d.ts
+++ src/components/common/Input/Input.d.ts
@@ -1,6 +1,7 @@
-import { InputHTMLAttributes, ReactNode } from 'react';
+import { InputHTMLAttributes, ReactNode } from "react"
 
 export interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
-	className?: string
-	icon?: ReactNode
+  className?: string
+  icon?: ReactNode
+  iconPosition?: "left" | "right"
 }
--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -1,13 +1,19 @@
-import { FC } from "react";
-import { IInputProps } from "./Input.d";
-import styles from "./Input.module.scss";
-import clsx from "classnames";
+import { FC } from "react"
+import { IInputProps } from "./Input.d"
+import styles from "./Input.module.scss"
+import clsx from "classnames"
 
-export const Input: FC<IInputProps> = ({ icon, className, ...props }) => {
+export const Input: FC<IInputProps> = ({
+  icon,
+  iconPosition = "left",
+  className,
+  ...props
+}) => {
   return (
     <div className={clsx(styles.wrapper, className)}>
-      {icon && icon}
+      {iconPosition === "left" && icon}
       <input className={styles.input} {...props} />
+      {iconPosition === "right" && icon}
     </div>
-  );
-};
+  )
+}
