39eaf732617ffd7f8d89ff4ac39c6aa22ee0e732
d33sf0me <katekrokh@gmail.com>
2025-08-28 13:25:24 +0300

    refactor: simplify ConfirmBuyNftBottomSheet by removing unused logic and reverting onBuy to a synchronous function

--- src/components/Modals/ConfirmBuyNftBottomSheet/ConfirmBuyNftBottomSheet.tsx
+++ src/components/Modals/ConfirmBuyNftBottomSheet/ConfirmBuyNftBottomSheet.tsx
@@ -1,17 +1,15 @@
-import { FC, useCallback } from "react"
-import { useBottomSheet } from "@/providers/BottomSheetProvider/BottomSheetProvider"
+import { FC } from "react"
 import styles from "./ConfirmBuyNftBottomSheet.module.scss"
 
 import questionMarkImg from "@/static/icons/question_mark.svg"
 import { Button } from "@/components/common/Button/Button"
-import { SuccessBottomSheet } from "../SuccessBottomSheet/SuccessBottomSheet"
 import Icon from "@/components/common/Icon/Icon"
 import { t } from "i18next"
 
 type Props = {
   nftPrice: number
   quantity: string
-  onBuy: () => Promise<void>
+  onBuy: () => void
   onCancel: () => void
 }
 
@@ -21,31 +19,6 @@ export const ConfirmBuyNftBottomSheet: FC<Props> = ({
   onCancel,
   quantity = 1,
 }) => {
-  const { openSheet, closeAll } = useBottomSheet()
-
-  const handleOnBuyClick = useCallback(async () => {
-    try {
-      await onBuy()
-      openSheet(
-        <SuccessBottomSheet
-          title="NFT успешно куплен"
-          subTitle="Мы уже отправили NFT к вам в профиль"
-          actionButtons={[
-            <Button type="primary" size="large" onClick={closeAll}>
-              Готово
-            </Button>,
-          ]}
-        />,
-        {
-          bottomSheetTitle: `${t("buy_nft")}`,
-        }
-      )
-    } catch (e) {
-      console.error(e)
-      //
-    }
-  }, [])
-
   return (
     <div className={styles.buyBottomSheetWrapper}>
       <Icon src={questionMarkImg} className={styles.buyBottomSheetImage} />
@@ -69,7 +42,7 @@ export const ConfirmBuyNftBottomSheet: FC<Props> = ({
         <Button type="secondary" onClick={onCancel} size="large">
           {t("buttons.cancel")}
         </Button>
-        <Button type="primary" onClick={handleOnBuyClick} size="large">
+        <Button type="primary" onClick={onBuy} size="large">
           {t("buttons.buy")}
         </Button>
       </div>
