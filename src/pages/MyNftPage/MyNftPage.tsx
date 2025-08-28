8599fc5dfc3c98f65afa99c8a82d2e6593327b79
d33sf0me <katekrokh@gmail.com>
2025-08-28 16:57:51 +0300

    refactor: update NFT handling in MyNftPage to improve gift selection and modal opening

--- src/pages/MyNftPage/MyNftPage.tsx
+++ src/pages/MyNftPage/MyNftPage.tsx
@@ -7,20 +7,52 @@ import { t } from "i18next"
 import { Outlet } from "react-router-dom"
 import { AddNftsBottomSheet } from "@/components/Modals/AddNftsBottomSheet/AddNftsBottomSheet"
 import { useBottomSheet } from "@/providers/BottomSheetProvider/BottomSheetProvider"
+import { SellNftModal } from "@/components/Modals/SellNftModal/SellNftModal"
+import { useGetMyGiftsQuery } from "@/api/endpoints/gifts"
 
 export const MyNftPage: FC = () => {
-  const { openSheet } = useBottomSheet()
+  const { openSheet, closeAll } = useBottomSheet()
 
-  const handleSellNft = () => {
-    openSheet(<AddNftsBottomSheet onConfirm={() => console.log("d")} />, {
-      bottomSheetTitle: t("adding_nfts"),
-    })
+  const { data: myGifts } = useGetMyGiftsQuery()
+
+  const handleAddNft = () => {
+    openSheet(
+      <AddNftsBottomSheet onConfirm={nftsIds => handleSellNft(nftsIds)} />,
+      {
+        bottomSheetTitle: t("adding_nfts"),
+      }
+    )
+  }
+
+  const handleSellNft = (nftsIds: string[]) => {
+    if (!myGifts) return
+
+    const selectedGifts = myGifts
+      .filter(gift => nftsIds.includes(gift.id))
+      .map(gift => ({
+        id: gift.id,
+        title: gift.model?.title ?? "",
+        number: gift.number ? String(gift.number) : "",
+        price: gift.price || 0.1,
+        preview: gift.preview_url ?? "",
+        background: gift.background_url ?? "",
+      }))
+
+    if (!selectedGifts.length) return
+    console.log("nft1sIds", selectedGifts)
+
+    closeAll() // закрыть AddNftsBottomSheet
+    setTimeout(() => {
+      openSheet(<SellNftModal nfts={selectedGifts} onSuccess={() => {}} />, {
+        bottomSheetTitle: t("sell_nft", "Продажа NFT"),
+      })
+    }, 0)
   }
   return (
     <div className={styles.root}>
       <div className={styles.header}>
         <p className={styles.title}>{t("my_nft")}</p>
-        <Button onClick={handleSellNft}>{t("buttons.sell_nft")}</Button>
+        <Button onClick={handleAddNft}>{t("buttons.sell_nft")}</Button>
       </div>
       {/* <Tabs>
         <Tab to="/my-nft/stickers">{t("stickers")}</Tab>
